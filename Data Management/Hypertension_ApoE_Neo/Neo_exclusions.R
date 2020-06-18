822c25b46236e85bd37f605ddb5bd529c5f6c5d4
Jennifer Collister <jennifer.collister@ndph.ox.ac.uk>
2020-06-18 17:06:37 +0100

    Made labelled factor variants of the new family history variables

--- Data Management/Hypertension_ApoE_Neo/Neo_exclusions.R
+++ Data Management/Hypertension_ApoE_Neo/Neo_exclusions.R
@@ -299,6 +299,10 @@ data$BowelCancerScreening <- factor(data$BowelCancerScreening, levels=c("Yes", "
 # Convert family history to a factor
 data$FamilyHist_CVD_ <- factor(as.numeric(data$FaH_CVD), levels=c(0,1), labels=c("No", "Yes"))
 
+data$FaH_HeartDisease_ <- factor(as.numeric(data$FaH_HeartDisease), levels=c(0,1), labels=c("No", "Yes"))
+data$FaH_Stroke_ <- factor(as.numeric(data$FaH_Stroke), levels=c(0,1), labels=c("No", "Yes"))
+data$FaH_Hypertension_ <- factor(as.numeric(data$FaH_Hypertension), levels=c(0,1), labels=c("No", "Yes"))
+
 # Convert income level of birth country to a factor
 data$BirthCountryIncomeLevel[data$BirthCountryIncomeLevel %in% c("LM", "UM")] <- "M"
 data$BirthCountryIncomeLevel <- factor(data$BirthCountryIncomeLevel, levels=c("HUK", "H", "M", "L"), 
