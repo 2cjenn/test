713870e00fe17681f0018cd7f14c75f2cde7f555
Jennifer Collister <jennifer.collister@ndph.ox.ac.uk>
2020-07-09 12:07:11 +0100

    Finished updating filepaths and variable names, altered all scripts to use config file so filepaths can be changed more easily

--- Statistical_Analysis/ApoE/MeetingUpdates/IncidenceRates.R
+++ Statistical_Analysis/ApoE/MeetingUpdates/IncidenceRates.R
@@ -5,12 +5,20 @@
 #--------------------------------------------------------------------------------------------------------------
 
 library(epitools)
+library(yaml)
+library(here)
 
-apoe <- readRDS(file="K:\\TEU\\APOE on Dementia\\Data Management\\R_Dataframes_TLA\\38358\\Organised\\apoe_excl.rds")
+
+# Load the project config file for filepaths etc
+config = yaml.load_file(here::here("./config.yml"))
+
+# Load data
+apoe <- readRDS(file=paste0(config$analysisdata$apoe_old, "apoe_excl.rds"))
+apoe$definedhypertension <- apoe$SBP >= 140 | apoe$DBP >= 90
 
 # Risk of dementia in each category
-case <- table(apoe$hypertension[apoe$status==TRUE], apoe$e4carrier[apoe$status==TRUE])
-pop <- table(apoe$hypertension, apoe$e4carrier)
+case <- table(apoe$definedhypertension[apoe$dementia_status==TRUE], apoe$e4carrier[apoe$dementia_status==TRUE])
+pop <- table(apoe$definedhypertension, apoe$e4carrier)
 inc <- round(100*case/pop,2)
 names(dimnames(inc)) <- c("Hypertension", "APOE e4 carrier")
 inc
@@ -20,12 +28,12 @@ apoe$agegrp <- cut(apoe$age, c(50, 55, 60, 65, 70), right=FALSE)
 # Number of individuals in each age group
 agepop <- table(apoe$agegrp)
 # Number of person-years in each age group
-pyears <- aggregate(apoe$ytime, by=list(apoe$agegrp), FUN=sum)
+pyears <- aggregate(apoe$time_to_dementia_yrs, by=list(apoe$agegrp), FUN=sum)
 
 
 # Crude incidence rate per age group in the whole population
 # Number of individuals in each age group by dementia status
-agedement <- table(apoe$agegrp, apoe$status)
+agedement <- table(apoe$agegrp, apoe$dementia_status)
 # Divide dementia cases by person-years in each age group
 wholepop <- agedement[,2]/(pyears[,2]/100000)
 
@@ -36,12 +44,12 @@ incrates <- c(wholepop)
 adjrates <- c()
 for (df in list(apoe[apoe$e4carrier==TRUE,], 
                 apoe[apoe$e4carrier==FALSE,],
-                apoe[apoe$hypertension==TRUE,],
-                apoe[apoe$hypertension==FALSE,])
+                apoe[apoe$definedhypertension==TRUE,],
+                apoe[apoe$definedhypertension==FALSE,])
 ) {
   
-  agedement <- table(df$agegrp, df$status)
-  pyears <- aggregate(df$ytime, by=list(df$agegrp), FUN=sum)
+  agedement <- table(df$agegrp, df$dementia_status)
+  pyears <- aggregate(df$time_to_dementia_yrs, by=list(df$agegrp), FUN=sum)
   
   # Crude incidence rates
   incrate <- round(agedement[,2]/(pyears[,2]/100000),2)
@@ -64,13 +72,13 @@ adjrates
 # Pair-wise groups
 incrates <- c(wholepop)
 adjrates <- c()
-for (df in list(apoe[apoe$e4carrier==FALSE & apoe$hypertension==FALSE,], 
-                apoe[apoe$e4carrier==TRUE & apoe$hypertension==FALSE,],
-                apoe[apoe$e4carrier==FALSE & apoe$hypertension==TRUE,],
-                apoe[apoe$e4carrier==TRUE & apoe$hypertension==TRUE,])
+for (df in list(apoe[apoe$e4carrier==FALSE & apoe$definedhypertension==FALSE,], 
+                apoe[apoe$e4carrier==TRUE & apoe$definedhypertension==FALSE,],
+                apoe[apoe$e4carrier==FALSE & apoe$definedhypertension==TRUE,],
+                apoe[apoe$e4carrier==TRUE & apoe$definedhypertension==TRUE,])
 ) {
-  agedement <- table(df$agegrp, df$status)
-  pyears <- aggregate(df$ytime, by=list(df$agegrp), FUN=sum)
+  agedement <- table(df$agegrp, df$dementia_status)
+  pyears <- aggregate(df$time_to_dementia_yrs, by=list(df$agegrp), FUN=sum)
   
   # Crude incidence rates
   incrate <- round(agedement[,2]/(pyears[,2]/100000),2)
