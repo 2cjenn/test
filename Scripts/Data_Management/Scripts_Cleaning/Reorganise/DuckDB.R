aa803252a6b021f3ffe66862ec4037610cdc3e2c
Jennifer Collister <jennifer.collister@ndph.ox.ac.uk>
2020-09-27 12:12:48 +0100

    Borrowed column type finder from ukbtools (parses html doc file). Got prototype (50,000 rows) working in DuckDB - note lowercase table names. Fread skip is slow for large skip values, consider use of awk to chunk file before reading.

--- Scripts/Data_Management/Scripts_Cleaning/Reorganise/DuckDB.R
+++ Scripts/Data_Management/Scripts_Cleaning/Reorganise/DuckDB.R
@@ -1,49 +1,102 @@
 library(tidyverse)
 library(DBI)
-library(vroom)
 library(duckdb)
+library(data.table)
+library(vroom)
+library(readr)
+
+col_type <- c(
+  "Sequence" = "integer",
+  "Integer" = "integer",
+  "Categorical (single)" = "character",
+  "Categorical (multiple)" = "character",
+  "Continuous" = "double",
+  "Text" = "character",
+  "Date" = "character",
+  "Time" = "character",
+  "Compound" = "character",
+  "Binary object" = "character",
+  "Records" = "character",
+  "Curve" = "character"
+)
+
+spec <- ukb_df_field(fileset = "ukb38358",
+                     path = "K:/TEU/UKB33952_Data/Data_Downloads/v2.0_B2006022_R38358") %>%
+  mutate(fread_column_type = col_type[col.type])
+
+
+df_func<- function(skip=0, nrows=25000, header=FALSE, col.names=NULL){
+  if(is.null(col.names)){
+    data.table::fread(
+      input = "K:/TEU/UKB33952_Data/Data_Downloads/v2.0_B2006022_R38358/R/Alldata/ukb38358.tab",
+      sep = "\t",
+      header = header,
+      colClasses = stringr::str_c(spec$fread_column_type),
+      data.table = FALSE,
+      showProgress = TRUE,
+      skip=skip,
+      nrows=nrows
+    ) 
+  } else{
+    data.table::fread(
+      input = "K:/TEU/UKB33952_Data/Data_Downloads/v2.0_B2006022_R38358/R/Alldata/ukb38358.tab",
+      sep = "\t",
+      header = header,
+      col.names=col.names,
+      colClasses = stringr::str_c(spec$fread_column_type),
+      data.table = FALSE,
+      showProgress = TRUE,
+      skip=skip,
+      nrows=nrows
+    ) 
+  }
+}
 
 con <- dbConnect(duckdb::duckdb(), "duck.db")
 
-df <- vroom("K:/TEU/UKB33952_Data/Data_Downloads/v2.0_B2006022_R38358/R/Alldata/ukb38358.tab", 
-            col_names=TRUE, delim="\t", 
-            n_max=25000)
-copy_to(con, df, name="UKB_1", overwrite=TRUE, temporary=FALSE)
-tbl1 <- tbl(con, from="UKB_1")
-data <- collect(tbl1)
-
-
 for(row in seq(1, 502520, by=25000)){
   
+  print(paste0("row ", row))
+  
   overwrite <- row==1
-  
-  for(col in seq(1, 9367,by=500)){
-    print(paste0("row ", row, ", col ", col))
-    
-    if(col==1){
-      cols <- c(1, seq(2, 500, by=1))
-    } else if (col==9001){
-      cols <- c(1, seq(9001, 9367, by=1))
-    } else {
-      cols <- c(1, seq(col, col+499, by=1))
+  if(overwrite){
+    # Coded "ok" (TRUE) as Don't overwrite and "cancel" (FALSE) as Do overwrite
+    # so that DEFAULT OPTION is NOT OVERWRITING!
+    dontOverwrite <- rstudioapi::showQuestion("Overwrite", 
+                                              message="Are you sure you want to overwrite this large, painstakingly created database???", 
+                                              ok="Uhoh, wait, no", 
+                                              cancel="Yup, I'm sure")
+    if(dontOverwrite){
+      stop("User decided not to overwrite")
     }
-    df <- vroom("K:/TEU/UKB33952_Data/Data_Downloads/v2.0_B2006022_R38358/R/Alldata/ukb38358.tab", 
-                col_names=TRUE, delim="\t", 
-                skip=row-1, n_max=25000, col_select=cols)
-    copy_to(con, df, name=paste0("UKB_", col), overwrite=overwrite, append=TRUE, temporary=FALSE)
   }
-}
-for(col in seq(1, 3500,by=500)){
-  tbl <- tbl(con, from=paste0("UKB_", col))
+  append <- row!=1
   
-  if(col==1){
-    pendingtable <- tbl
-  } else if (col > 1) {
-    pendingtable <- inner_join(pendingtable, tbl, by="f.eid")
+  if(row==1){
+    df <- df_func(header=TRUE)
+    
+    col.names <- colnames(df)
+  } else if (row>1){
+    df <- df_func(skip=row, col.names=col.names)
   }
+  
+  print("read")
+  
+  dbWriteTable(con, df, name="ukb", overwrite=overwrite, append=append, temporary=FALSE)
+  
+  print("written")
+
 }
 
-db <- pendingtable %>% select(f.eid, f.21.0.0, f.35.0.0) %>% collect
-db <- collect(pendingtable)
+dbListTables(con)
+
+qry <- dbGetQuery(con, "SELECT COUNT(*) FROM ukb")
+
+df <- tbl(con, from="ukb") %>% 
+  select(f.eid, f.21.0.0, f.34.0.0, starts_with("f.84."), starts_with("f.87.")) %>% 
+  collect
+
+
+
 
 dbDisconnect(con)
