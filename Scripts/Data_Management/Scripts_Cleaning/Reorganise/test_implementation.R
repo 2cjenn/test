8ac74a789c2ce2df32c61e8090fffcad61ab7a28
Jennifer Collister <jennifer.collister@ndph.ox.ac.uk>
2020-10-02 11:21:31 +0100

    Added database path to the config file

--- Scripts/Data_Management/Scripts_Cleaning/Reorganise/test_implementation.R
+++ Scripts/Data_Management/Scripts_Cleaning/Reorganise/test_implementation.R
@@ -9,6 +9,6 @@ config = yaml.load_file("config.yml")
 
 source(file.path(config$scripts$cleaning, "Reorganise", "dataset_generator.R"))
 
-data <- derive_variables(database = "ukb_v2.db", field_definitions = testSpecs$SPEC_Neo_HTN)
+data <- derive_variables(database = config$data$database, field_definitions = testSpecs$SPEC_Neo_HTN)
 
-thing <- testSpecs$SPEC_Neo_HTN
+thing <- TEU_SPECS$SPEC_Neo_HTN
